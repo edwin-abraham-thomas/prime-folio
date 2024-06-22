21de1be287a49d78af9a81002069c7e66928a185
Edwin Abraham Thomas <edwinabrahamthomas@outlook.com>
2024-06-22 16:38:26 +0530

    base url update

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,5 +5,5 @@ export const environment = {
     auth: {
         ...local
     },
-    apiBaseURL: 'https://localhost:7247'
+    apiBaseURL: 'https://pfz3a4ncgb.execute-api.ap-south-1.amazonaws.com'
 };
